e36f4e359e87fa3d1e3d41eb489411ffc50a7011
lsk <lsk920110@gmail.com>
2022-11-22 15:07:36 +0900

    initialize #1 - java project
    지난 프로젝트를 복사해옴

---
+++ src/test/java/com/fastcampus/javaoop/logic/JavaSortTest.java
@@ -0,0 +1,24 @@
+package com.fastcampus.javaoop.logic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaSortTest {
+    @DisplayName("자바소트 - 리스트를 넣으면 정렬된 결과를 보여줌")
+    @Test
+    void given_List_WhenExecuting_ThenReturnSortedList(){
+
+         JavaSort<Integer> javaSort = new JavaSort<>();
+
+        List<Integer> actual = javaSort.sort(List.of(3, 2, 4, 5, 1));
+
+        assertEquals(List.of(1,2,3,4,5),actual);
+
+        System.out.println("미아러나ㅣㅇ허ㅏㅣㄴ");
+    }
+
+}
