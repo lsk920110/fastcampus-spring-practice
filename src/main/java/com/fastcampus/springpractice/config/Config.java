acf67e51aa69807fcf8cec1d5feb3b8a0bc6c540
lsk <lsk920110@gmail.com>
2022-11-22 16:05:10 +0900

    Config #1 - spring configuration
    기초설정 , 루트 패키지 이하에 빈이 있는지 스캔한다.

---
+++ src/main/java/com/fastcampus/springpractice/config/Config.java
@@ -0,0 +1,12 @@
+package com.fastcampus.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.fastcampus.springpractice")
+//스프링 전용 컨피그 클래스가 되었음
+@Configuration
+public class Config {
+
+
+}
